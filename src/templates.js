27cf823dea1e5a0938e42c67a12a5293bc4c1a1c
Nathan <ngrasset@student.42.fr>
2016-09-08 10:33:41 +0200

    make the package an executable

--- src/templates.js
+++ src/templates.js
@@ -11,11 +11,10 @@ module.exports = class %s extends React.Component {
 }
 `
 
-const classify = str => {
-	return str.split(/[-_ ]/).map(part => {
-			return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
-		}).join('')
-}
+const classify = str => str
+	.split(/[-_ ]/)
+	.map(part => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase())
+	.join('')
 
 module.exports = {
 	component: (name) => util.format(COMPONENT_TEMPLATE, classify(name))
